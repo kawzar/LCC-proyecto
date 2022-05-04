c19beb82c48b965bcc6dfb877fd4debf644fb57c
Marlow Haspert <marlowhaspert@gmail.com>
2022-05-04 19:00:47 -0300

    find

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -9,10 +9,34 @@
 %
 % FGrid es el resultado de hacer 'flick' de la grilla Grid con el color Color.
 % Retorna false si Color coincide con el color de la celda superior izquierda de la grilla. 
-
-flick(Grid, Color, FGrid):-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+flick(Grid, Color, FGrid) :-
 	Grid = [F|Fs],
 	F = [X|Xs],
 	Color \= X,
 	FGrid = [[Color|Xs]|Fs].
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% findInGrid(+Grid, +I, +J, -Element)
+%
+% Element es el elemento que se encuentra en la posicion dada por el vector [I, J]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+findInGrid([G|_], I, J, Element) :-
+	findInRow(G, I,Element).
+findInGrid([_|Grid], I, J,Element) :-
+	Y is J-1,
+	findInGrid(Grid, I, Y, Element).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% findInRow(+Row, +I, -Element)
+%
+% Element es el elemento que se encuentra en la posicion Index de la fila Row
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+findInRow([L|_], Index, L).
+findInRow([_|Ls],Index, Element) :-
+	X is Index-1,
+	findInRow(Ls, X, Element).
